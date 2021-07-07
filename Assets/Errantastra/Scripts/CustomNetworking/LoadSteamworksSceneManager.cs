36e545e9287d198aec49299b9806d78391d542e8
jamesleakos <jamesleakos@gmail.com>
2021-07-06 23:21:07 -0700

    messing around with trying to get leaving the game wroking

---
+++ Assets/Errantastra/Scripts/CustomNetworking/LoadSteamworksSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSteamworksSceneManager : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("StartScreen");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
